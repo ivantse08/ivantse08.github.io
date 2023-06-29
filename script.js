c909d319142316a81763e748f7165ea04f03f942
ivantse08 <ivantse100@gmail.com>
2023-06-28 18:17:48 -0700

    changed file path

---
+++ script.js
@@ -0,0 +1,18 @@
+var dropdownButton = document.querySelector('.dropdown-button');
+var dropdownContent = document.querySelector('.dropdown-content');
+
+dropdownButton.addEventListener('click', function() {
+  if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+  } else {
+    dropdownContent.style.display = 'block';
+  }
+});
+
+// Optional: Close the dropdown when clicking outside of it
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('.dropdown-button')) {
+    dropdownContent.style.display = 'none';
+  }
+});
+
